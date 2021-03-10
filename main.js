24a080b260bdf4e47573b020e41ea13e769d0416
Yuktha0208 <71544862+Yuktha0208@users.noreply.github.com>
2021-03-10 17:47:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    canvas=createCanvas(400,400);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snap(){
+    save("filter_img_saved.jpeg");
+    console.log("Filter Saved");
+}
